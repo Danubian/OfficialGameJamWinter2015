b653a44fb566da15c33ff2c8353f18938c3c93ec
Danubian <stjohnzack@yahoo.com>
2015-01-24 12:50:10 -0500

    Add Win screen

--- Assets/Scripts/GlobalVar.cs
+++ Assets/Scripts/GlobalVar.cs
@@ -5,6 +5,7 @@ public class GlobalVar : Singleton<GlobalVar> {
 
 	public float PLAYER_SCORE_1 = 0f;
 	public float PLAYER_SCORE_2 = 0f;
+	public int MAX_PICKUPS = 5;
 	public float MAX_DIST = 30f;
 	public float PLAYER_SPEED = 15f;
 	public float MAX_PLAY_TIME = 90f;
---
+++ Assets/Scripts/PickupController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupController : MonoBehaviour {
+	public int value = 100;
+
+	public const string NOTHING_STATE = "NOTHING_STATE";
+	public const string FLAMINGO_STATE = "FLAMINGO_STATE";
+	public const string FOX_STATE = "FOX_STATE";
+
+	public Material[] materials;
+	private Transform glowCube;
+
+	public string currentState = NOTHING_STATE;
+	// Use this for initialization
+	void Start () {
+		int chosenState = Random.Range(0, 3);
+		switch(chosenState)
+		{
+		case 0:
+			currentState = NOTHING_STATE;
+
+			break;
+		case 1:
+			currentState = FLAMINGO_STATE;
+			break;
+		case 2:
+			currentState = FOX_STATE;
+			break;
+		}
+
+		glowCube = this.transform.FindChild ("GlowCube");
+		glowCube.renderer.material = materials [chosenState];
+
+
+	}
+
+	void OnTriggerEnter (Collider other) {
+		Debug.Log (other.name);
+
+		if(other.name.Equals("Fox") && currentState == FOX_STATE)
+		{
+			Debug.Log("Fox Ding!");
+			GlobalVar.Instance.PLAYER_SCORE_1 += value;
+			Destroy(this.gameObject);
+		}
+		else if (other.name.Equals("Flamingo") && currentState == FLAMINGO_STATE)
+		{
+			Debug.Log("Flamingo Ding!");
+			GlobalVar.Instance.PLAYER_SCORE_2 += value;
+			Destroy(this.gameObject);
+		}
+
+		if(GlobalVar.Instance.PLAYER_SCORE_1 == GlobalVar.Instance.MAX_PICKUPS*value || 
+		   GlobalVar.Instance.PLAYER_SCORE_2 == GlobalVar.Instance.MAX_PICKUPS*value)
+		{
+			Application.LoadLevel(2);
+		}
+
+	}
+}
