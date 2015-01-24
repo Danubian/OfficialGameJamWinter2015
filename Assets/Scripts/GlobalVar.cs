f47fc18e4bebab323269ee8e985ba9a24beb3daa
Danubian <stjohnzack@yahoo.com>
2015-01-24 15:55:48 -0500

    Adding art

--- Assets/Scripts/GlobalVar.cs
+++ Assets/Scripts/GlobalVar.cs
@@ -6,7 +6,7 @@ public class GlobalVar : Singleton<GlobalVar> {
 	public float PLAYER_SCORE_1 = 0f;
 	public float PLAYER_SCORE_2 = 0f;
 	public int MAX_PICKUPS = 5;
-	public float MAX_DIST = 50f;
+	public float MAX_DIST = 20f;
 	public float PLAYER_SPEED = 15f;
 	public float MAX_PLAY_TIME = 30f;	
 	public float PLAYER_ROTATE_SMOOTH = 10f;
